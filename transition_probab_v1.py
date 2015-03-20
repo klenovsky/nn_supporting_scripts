8d5bc6f6f3b3f296819553c4cc71a287c64539d7
DOMINIQUE GEFFROY <geffroy.dominique@free.fr>
2015-03-20 14:02:10 +0100

    intermediate commit - we are getting there, but the naming scheme....

--- transition_probab_v1.py
+++ transition_probab_v1.py
@@ -13,6 +13,11 @@ e=1.602176565 #*10**(-19)
 print na.__version__
 
 class trans_probab():
+
+  REAL_ROOT = 'wf_amplitude_real_dot_'
+  IMAG_ROOT = 'wf_amplitude_imag_dot_'
+
+      
   def __init__(self , prefix='' , Ep=21.5 , kind='kp8' ):
     print 'transition_probab initialized'
     self.prefix=prefix
@@ -45,7 +50,6 @@ class trans_probab():
   def readSizesFromFld( self, state , wftype ):
     #print state
     fldname= os.path.join(self.prefix, 'wf_amplitude_real_dot_'+wftype+'_1_0000_00%02i.fld'%(state))
-
     #print fldname
     f=open(fldname,'r')
     lines=[ f.readline() for i in xrange(8) ]
@@ -57,37 +61,28 @@ class trans_probab():
     #print 'Sizes %d, %d, %d readed from fld file (%d in total)' % \
     #  (self.nx, self.ny, self.nz, self.n)
 
-
-
-
   #Funkce generujici Ep jako funkci prostorove souradnice, pouzita je linearni interpolace mezi prislusnymi konstituenty
   def makeEp( self , coordNameToFit , state1=8 , state2=9 , wftype1='kp8' , wftype2='kp8' ):
     #print    
     alloyname = os.path.join(self.prefix, 'alloy_composition.dat')
     fldname=os.path.join(self.prefix, 'alloy_composition.fld')
     coordname=os.path.join(self.prefix, 'alloy_composition.coord')
-
     dim=self.readDimensions( fldname )
     nx=dim[0]
     ny=dim[1]
     nz=dim[2]
     n=dim[3]
-
     #print nx,ny,nz,nx*ny*nz
     #print self.nx,self.ny,self.nz,self.nx*self.ny*self.nz
-
     data=self.read_data_from_NN( alloyname )[0:n]
-    
     coord=self.read_data_from_NN( coordname )[0:n]    
     coordFit=self.read_data_from_NN( coordNameToFit )[0:self.n]
-        
     #Inline funkce, ktera provadi cele pretypovani, mezi QD a SRL rozlisuje typickou limitou koncentrace slitiny (<0.35 SRL, >0.35 QD)
     #PREFFERED FOR III-V
     EpTemp=[(x*27.0+(1.0-x)*28.8) if x>0.35 else (x*21.5+(1.0-x)*28.8) for x in (data)]    
     #InAs Ep=21.5 eV
     #GaAs Ep=28.8 eV
     #GaSb Ep=27.0 eV
-
     #Si_(1-x)Ge_x QD attempt using method by Cardona (Fundamentals of Semiconductors)
     #Eab=abs(self.retrieveWFEnergy( state1 , wftype1 )-self.retrieveWFEnergy( state2 , wftype2 ))
     #print 'Eab ', Eab
@@ -98,12 +93,10 @@ class trans_probab():
     #EgGe=1.2 #for X point source: Ioffe
     #so far phonon energy is subtracted, so phonon is considered as emitted, which should be the case for low temperatures
     #EpTemp=[(Eab - (x*EphGe+(1.0-x)*EphSi) - (x*EgGe+(1.0-x)*EgSi) )**2.0*na.pi/8.0 for x in (data)]
-    
     #Si_(1-x)Ge_x QD attempt using data from Richard
     #EpTemp=[(x*12.23+(1.0-x)*14.81) for x in (data)]
     #Si Ep=14.81 eV   E_PX from Richard PHYSICAL REVIEW B 70, 235204 (2004)
     #Ge Ep=12.23 eV   E_PXd from Richard PHYSICAL REVIEW B 70, 235204 (2004)
-
     #Si_(1-x)Ge_x QD attempt using method from Cardona and data from Richard <del|p|gam>=sqrt( 2*pi/hbar*10^-4*(|<gam_15|p|gam_25'>|^2*|<gam_15|del_1>|^2)/(Egdel-Etrans)^2 )
     #PREFFERED FOR SIGE
     #EpxSi=14.81 #from Richard 30 band k.p
@@ -115,41 +108,37 @@ class trans_probab():
     #EpTemp=[ na.sqrt( 2.0*na.pi/hbar*10**(-4.0)*(x*EpxGe+(1.0-x)*EpxSi)**2.0*(x*0.7+(1.0-x)*0.75)**2.0/( abs((x*EgGe+(1.0-x)*EgSi)-Eab) )**2.0 ) for x in (data)]
     #Si Ep=14.81 eV   E_PX from Richard PHYSICAL REVIEW B 70, 235204 (2004)
     #Ge Ep=12.23 eV   E_PXd from Richard PHYSICAL REVIEW B 70, 235204 (2004)
-
     #Si_(1-x)Ge_x QD attempt using data from Richard <delta_1|p|gamma_25> = <gamma_15|p|gamma_25>*<gamma_15|delta_1>
     #EpxSi=14.81 #E_PX from Richard PHYSICAL REVIEW B 70, 235204 (2004)
     #EpxGe=12.23 #E_PXd from Richard PHYSICAL REVIEW B 70, 235204 (2004)
     #EpTemp=[(x*EpxGe+(1.0-x)*EpxSi)*(x*0.7+(1.0-x)*0.75) for x in (data)]
     #Si Ep=14.81 eV   E_PX from Richard PHYSICAL REVIEW B 70, 235204 (2004)
     #Ge Ep=12.23 eV   E_PXd from Richard PHYSICAL REVIEW B 70, 235204 (2004)
-
-
-
     EpUncut=na.asarray(EpTemp).reshape((nx, ny, nz), order='FORTRAN')
     UncutCoords=na.asarray([ coord[:nx] , coord[nx:nx+ny] , coord[nx+ny:nx+ny+nz] ])
     UncutFitCoords=na.asarray([ coordFit[:self.nx] , coordFit[self.nx:self.nx+self.ny] , coordFit[self.nx+self.ny:self.nx+self.ny+self.nz] ])
-
     #Function for interpolating Ep to the grid of wavefunctions 
-    
-    
     lo=na.array([ UncutCoords[0][0], UncutCoords[1][0], UncutCoords[2][0] ])
     hi=na.array([ UncutCoords[0][-1], UncutCoords[1][-1], UncutCoords[2][-1] ])
     #uniforming coordinates
     maps=[UncutCoords[0],UncutCoords[1],UncutCoords[2]]
     #calling spline interpolation
     interfunc = Intergrid( EpUncut, lo=lo, hi=hi, order=0 )
-        
     #performing spline interpolation
     query_points=[([x,y,z]) for x in UncutFitCoords[0] for y in UncutFitCoords[1] for z in UncutFitCoords[2] ]
-                 
     Ep=interfunc.at( query_points ).reshape(self.nx,self.ny,self.nz)
     print 'INTERFUNC DIM ',Ep.ndim
     print 'Ep GENERATED'
-
     return Ep
 
-
-
+  def get_wf_names(self, state, wftype='kp8'):
+        real_suffixes = [self.REAL_ROOT + wftype + '_' + str(i) +
+                         '_0000_00%02i.dat'%(state) for i in xrange(1,9)]
+        imag_suffixes = [self.IMAG_ROOT + wftype + '_' + str(i) +
+                         '_0000_00%02i.dat'%(state) for i in xrange(1,9)]
+        real_names = [os.path.join(self.prefix, elem) for elem in real_suffixes] 
+        imag_names = [os.path.join(self.prefix, elem) for elem in imag_suffixes] 
+        return (real_names, imag_names)
 
   #Funkce generujici jmena 8kp wf souboru, stejne jako v strain_from_nextnano_to_csi.py
   def generateWFNames( self , state , wftype='kp8' ):
@@ -180,12 +169,6 @@ class trans_probab():
          #imag=[ self.path+'wf_amplitude_imag_dot_x3_1_'+'_'.join(parts[6:])+'.dat' ]
          self.allDatnames=real
 
-    #print real
-    #return allDatnames
-
-
-
-    
   #Funkce vytvarejici 8kp wf z nextnana do formatu 8kpTUB, stejne jako v strain_from_nextnano_to_csi.py
   def makeWFkp8state ( self , wftype='kp8' , sbSpin=0 ): #, wftype='kp8' 
     #print self.allDatnames
@@ -231,37 +214,71 @@ class trans_probab():
     #data.append(totaldens)
     return data                                 
 
-  def make_alternate_WFkp8state (self, wftype='kp8', sbSpin=0 ): #, wftype='kp8'
-    nx=self.nx
-    ny=self.ny
-    nz=self.nz 
-    buffers=[ self.read_data_from_NN( self.datname ).reshape((nx, ny, nz), order='FORTRAN') for self.datname in self.allDatnames ]
+  def make_complex_buffer(self, name_real, name_imag):
+        out = (na.fromfile(name_real,  sep='\n')
+              + na.fromfile(name_imag,  sep='\n') * 1j)
+        return out, na.sum(na.absolute(out)**2)
+
+  def get_state_vectors(self, buffer_names):
+    norm_squared = 0.0
+    sup_values, modulus = self.make_complex_buffer(buffer_names[0], buffer_names[8])
+    norm_squared += modulus
+    sdown_values, modulus = self.make_complex_buffer(buffer_names[1], buffer_names[9])
+    norm_squared += modulus    
+    all_x_up, modulus = self.make_complex_buffer(buffer_names[2], buffer_names[10])
+    norm_squared += modulus
+    all_y_up, modulus = self.make_complex_buffer(buffer_names[3], buffer_names[11])
+    norm_squared += modulus    
+    all_z_up, modulus = self.make_complex_buffer(buffer_names[4], buffer_names[12])
+    norm_squared += modulus
+    all_x_down, modulus = self.make_complex_buffer(buffer_names[5], buffer_names[13])
+    norm_squared += modulus    
+    all_y_down, modulus = self.make_complex_buffer(buffer_names[6], buffer_names[14])
+    norm_squared += modulus    
+    all_z_down, modulus = self.make_complex_buffer(buffer_names[7], buffer_names[15])
+    norm_squared += modulus    
+    norm = na.sqrt(norm_squared)
+    return (sup_values / norm,
+            sdown_values / norm,
+            all_x_up / norm,
+            all_y_up / norm,
+            all_z_up / norm,
+            all_x_down / norm,
+            all_y_down / norm,
+            all_z_down / norm)
+
+    
+  def make_intermediate_vector(self, buffer_names):
     # ordering in this wf: real amplitudes + imag amplitudes + total density; ordering in nn++ wf: s up, s down, x, y, z up, z, y, z down
     # final ordering: s up, x, y, z up, s down, z, y, z down      real first 8 
-    if (wftype=='kp8'): 
-      data = na.array([buffers[0] + buffers[8]*1j,
-                       buffers[2] + buffers[10]*1j,
-                       buffers[3]  + buffers[11]*1j,
-                       buffers[4]  + buffers[12]*1j,
-                       buffers[1]  + buffers[9]*1j,
-                       buffers[5]  + buffers[13]*1j,
-                       buffers[6]  + buffers[14]*1j,
-                       buffers[7] + buffers[15]*1j], 'complex')
-    else:
-        raise ValueError("ooooops, unrecognized wftype")
-    norm = na.sum(na.absolute(data))
-    data /= norm
-    sup_values = data[0].ravel()
-    sdown_values= data[4].ravel()
-    all_x_up = data[1].ravel()
-    all_y_up = data[2].ravel()
-    all_z_up = data[3].ravel()
-    fup_vectors = na.column_stack((all_x_up, all_y_up, all_z_up))
-    all_x_down = data[5].ravel()
-    all_y_down = data[6].ravel()
-    all_z_down = data[7].ravel()    
-    fdown_vectors = na.column_stack((all_x_down, all_y_down, all_z_down))
-    return (fup_vectors, fdown_vectors, sup_values, sdown_values)
+    real_names_1 = buffer_names[0]
+    imag_names_1 = buffer_names[1]
+    real_names_2 = buffer_names[2]
+    imag_names_2 = buffer_names[3]
+    # Build the vectors from buffers, coordinate per coordinate.
+    sup_values_1, sdown_values_1, all_x_up_1, all_y_up_1, all_z_up_1,\
+      all_x_down_1, all_y_down_1, all_z_down_1 = self.get_state_vectors(real_names_1 + imag_names_1)
+    sup_values_2, sdown_values_2, all_x_up_2, all_y_up_2, all_z_up_2,\
+      all_x_down_2, all_y_down_2, all_z_down_2 = self.get_state_vectors(real_names_2 + imag_names_2)
+    # Execute the vector operations leading to the intermediate vector,
+    # Row by row, mixing spin and space variables,
+    # and initial as final states following Eq. 1.26 from Klenovsky's PhD thesis.
+    all_x_up_2 *= sup_values_1
+    all_y_up_2 *= sup_values_1
+    all_z_up_2 *= sup_values_1
+    all_x_down_2 *= sdown_values_1
+    all_y_down_2 *= sdown_values_1
+    all_z_down_2 *= sdown_values_1
+    all_x_up_1 = (sup_values_2 * all_x_up_1).conjugate()
+    all_y_up_1 = (sup_values_2 * all_y_up_1).conjugate()    
+    all_z_up_1 = (sup_values_2 * all_z_up_1).conjugate()
+    all_x_down_1 = (sdown_values_2 * all_x_down_1).conjugate()
+    all_y_down_1 = (sdown_values_2 * all_y_down_1).conjugate()    
+    all_z_down_1 = (sdown_values_2 * all_z_down_1).conjugate()
+    intermediate_vector = na.column_stack((all_x_up_2  + all_x_down_2 - all_x_up_1 - all_x_down_1,
+                                           all_y_up_2  + all_y_down_2 - all_y_up_1 - all_y_down_1,
+                                           all_z_up_2  + all_z_down_2 - all_z_up_1 - all_z_down_1))
+    return intermediate_vector
 
   def cache_quantities(self):
     pass
@@ -387,34 +404,32 @@ class trans_probab():
     hh_re=1/na.sqrt(2.0)*real[1]
     hh_im=1/na.sqrt(2.0)*imag[2] 
 
+  def get_transition_weights(self):
+        out = na.array([])
+        for transition in st.states:
+            na.append(out, float(transition[2]))
+        return out
 
-  def load_alternate_WFs(self , wft1 , wft2):
-    wfStates = []
-    stWeight = []
-    for s in range(len(st.states)):
-         print 'STATES', st.states[s][0], st.states[s][1]
-         stWeight.append(float(st.states[s][2]))
-         if len(st.states[s][0].split('_')) == 1:
-            st1=int(st.states[s][0])
-            st2=int(st.states[s][1])
-            spin=0          
-            #TME.append(self.makeTME (st1 , st2 , wft1 , wft2))
-         elif len(st.states[s][0].split('_')) == 2:
-            st1=int(st.states[s][0].split('_')[0])
-            spin=int(st.states[s][0].split('_')[1])
-            st2=int(st.states[s][1])
-            #TME.append(self.makeTME (st1 , st2 , wft1 , wft2 , spin=Spin1))
-         #Nacteni obou wf:
-         #state 1
-         self.readSizesFromFld(st1 , wft1)
-         self.generateWFNames (st1 , wft1)
-         alternate_kp8_1 = self.make_alternate_WFkp8state(wft1, sbSpin=spin)
-         #state 2
-         self.readSizesFromFld(st2 , wft2)
-         self.generateWFNames (st2 , wft2)
-         alternate_kp8_2 = self.make_alternate_WFkp8state(wft2)
-         wfStates.append([alternate_kp8_1 , alternate_kp8_2])
-    return (wfStates, stWeight)
+  def load_alternate_WFs(self , wft1 , wft2, transition):
+        state_1 = transition[0]
+        state_2 = transition[1]
+        print 'Handling transition', state_1, state_2
+        if len(state_1.split('_')) == 1:
+              st1 = int(state_1)
+        elif len(state_1.split('_')) == 2:
+              st1 = int(state_1.split('_')[0])
+        st2 = int(state_2)
+        # Nacteni obou wf:
+        # state 1
+        # st1 and st2 have be same sizes, otherwise we will get into trouble later,
+        # so only need to read sizes once.
+        self.readSizesFromFld(st1 , wft1)
+        real_names_1, imag_names_1 = self.get_wf_names(st1, wft1)
+        real_names_2, imag_names_2 = self.get_wf_names(st2, wft2)
+        transition_names = (real_names_1, imag_names_1,
+                            real_names_2, imag_names_2)
+        intermediate_vector = self.make_intermediate_vector(transition_names)
+        return intermediate_vector
   
   def loadWFs( self , wft1 , wft2):
     wfStates = []
@@ -449,16 +464,18 @@ class trans_probab():
   # THE SAME TRANSITION ENERGY BETWEEN STATES FOR CALCULATION OF EP
   # different types of basis: basisType='sppp' or basisType='shls'
   def makePolDep(self, pol=[[1, 1, 0]], basisType='sppp'):
-    local_mode = 'classical'
+    local_mode = 'alternate'
     print '\nMaking transition matrix elements\n'
-
     wft1 = st.wftype[0]
     wft2 = st.wftype[1]
+    stWeights = self.get_transition_weights()
+    WeightNorm = na.sum(stWeights)    
+    kp8wf = None
     if local_mode == 'alternate':
-      kp8wf, stWeight = self.load_alternate_WFs(wft1, wft2)
+      pass
     else:
       kp8wf, stWeight = self.loadWFs(wft1, wft2)
-    WeightNorm = na.sum(stWeight)
+      WeightNorm = na.sum(stWeight)
 
     # Eab=abs(self.retrieveWFEnergy( st1 , wft1 )
     # -self.retrieveWFEnergy(st2, wft2))
@@ -473,6 +490,7 @@ class trans_probab():
     elif len(st.states[0][1].split('_')) == 2:
             st1 = int(st.states[0][1].split('_')[0])
     coordForEp = self.allDatnames[0][:-4] + '.coord'
+    print "coordForEp", coordForEp
     Ep = self.makeEp(coordForEp, st1, st2, wft1, wft2)
     # Prepocet Ep na P
     P = sp.sqrt(Ep)
@@ -505,29 +523,20 @@ class trans_probab():
         Fel1 = []
         Fel2 = []
         Fel3 = []
-        for i in range(len(kp8wf)):
+        for i in range(len(st.states)):
+        #for i in range(len(kp8wf)):
             if basisType == 'sppp':
-              if local_mode == 'alternate':
-                  sup_values_i = kp8wf[i][0][0]
-                  fup_vectors_i = kp8wf[i][0][1]
-                  sdown_values_i = kp8wf[i][0][2]
-                  fdown_vectors_i = kp8wf[i][0][3]
-                  sup_values_f = kp8wf[i][1][0]
-                  fup_vectors_f = kp8wf[i][1][1]
-                  sdown_values_f = kp8wf[i][1][2]
-                  fdown_vectors_f = kp8wf[i][1][3]
-                  # TODO: check correct account of direction of scalar product
-                  # with conjugation, compared to sign of imaginary part in
-                  # classical implementation.
-                  intermediate_vector = sup_values_i * fup_vectors_f
-                  intermediate_vector += sdown_values_i[:,None] * fdown_vectors_f
-                  intermediate_vector -= (sup_values_f[:,None] * fup_vectors_i).conjugate()
-                  intermediate_vector -=(sdown_values_f[:,None] * fdown_vectors_i).conjugate()
-                  TME.append(self.make_alternate_TME(iter, intermediate_vector))
-              else:
-                  TME.append(self.makeTME(kp8wf[i][0], kp8wf[i][1], iter))
+                if local_mode == 'alternate':
+                    intermediate_vector =\
+                      self.load_alternate_WFs(st.states[i], wft1, wft2)
+                    # TODO: check correct account of direction of scalar product
+                    # with conjugation, compared to sign of imaginary part in
+                    # classical implementation.
+                    TME.append(self.make_alternate_TME(iter, intermediate_vector))
+                else:
+                    TME.append(self.makeTME(kp8wf[i][0], kp8wf[i][1], iter))
             elif basisType == 'shls':
-                TME.append(self.makeTMEhls(kp8wf[i][0], kp8wf[i][1], iter))
+                    TME.append(self.makeTMEhls(kp8wf[i][0], kp8wf[i][1], iter))
             MEreal = TME[i][0][0] + TME[i][0][1] + TME[i][0][2]
             MEimag = TME[i][1][0] + TME[i][1][1] + TME[i][1][2]
             sumOfME = (na.sum(P*MEreal))**2+(na.sum(P*MEimag))**2
